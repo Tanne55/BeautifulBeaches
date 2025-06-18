91ac4873fc21bc32104cb71eb3092dfdaa1cfca6
pham-thanh-nhan <phamthanhnhankk123@gmail.com>
2025-06-18 16:40:21 +0700

    Review đã lên sóng, tha hồ bóc phốt bãi biển

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
+
     return view('hello ');
 });
+
+Route::get('/reviews', [ReviewController::class, 'index']);
+Route::post('/reviews', [ReviewController::class, 'store']);
